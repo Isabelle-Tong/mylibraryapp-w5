bf327e069d909c6259870ea5b79d6bfaa0a065fb
Isabelle-Tong <61863144+Isabelle-Tong@users.noreply.github.com>
2020-04-22 12:26:43 +1000

    new

---
+++ models/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose");
+
+// Connect to MongoDB --- Replace this with your Connection String
+CONNECTION_STRING = "mongodb+srv://Isabelle:TZtz05140608@cluster0-p3t1v.mongodb.net/test?retryWrites=true&w=majority";
+
+MONGO_URL = CONNECTION_STRING.replace("TZtz05140608",process.env.MONGO_PASSWORD);
+
+console.log(MONGO_URL);
+
+mongoose.connect(MONGO_URL || "mongodb://localhost/info30005", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    dbName: "mylibraryapp"
+});
+
+const db = mongoose.connection;
+db.on("error", err => {
+    console.error(err);
+    process.exit(1);
+});
+db.once("open", async () => {
+    console.log("Mongo connection started on " + db.host + ":" +
+        db.port);
+});
+require("./author");
